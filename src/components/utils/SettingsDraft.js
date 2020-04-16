8baaeeffffbec5f9ce5ed98856d1c1e06bbdfbf8
Merlin Laffitte <merlin.laffitte@student.ecp.fr>
2020-04-16 11:33:44 +0200

    Remove prettier, use pycharm google settings instead

--- src/components/utils/SettingsDraft.js
+++ src/components/utils/SettingsDraft.js
@@ -1,8 +1,7 @@
-
-import PrimaryText from '../PrimaryText'
-import { withStyles } from '@material-ui/core/styles';
+import PrimaryText from '../PrimaryText';
+import {withStyles} from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
-import Tooltip from "@material-ui/core/Tooltip/Tooltip";
+import Tooltip from '@material-ui/core/Tooltip/Tooltip';
 
 const ACTIVE_MARKS = [
   {
@@ -24,12 +23,12 @@ const ACTIVE_MARKS = [
 ];
 
 function ValueLabelComponent(props) {
-  const {open, children, value } = props;
+  const {open, children, value} = props;
 
   return (
-    <Tooltip open={open} enterTouchDelay={0} placement="bottom" title={value}>
-      {children}
-    </Tooltip>
+      <Tooltip open={open} enterTouchDelay={0} placement="bottom" title={value}>
+        {children}
+      </Tooltip>
   );
 }
 
@@ -74,32 +73,47 @@ const AirbnbSlider = withStyles({
 
 function AirbnbThumbComponent(props) {
   return (
-    <span {...props}>
-      <span className="bar" />
-      <span className="bar" />
-      <span className="bar" />
+      <span {...props}>
+      <span className="bar"/>
+      <span className="bar"/>
+      <span className="bar"/>
     </span>
   );
 }
 
-    handleSliderChange(event, value){
-        const beginHour = value[0];
-        const endHour = value[1]
-        this.setState({ beginHour });
-        this.setState({ endHour });
+handleSliderChange(event, value);
+{
+  const beginHour = value[0];
+  const endHour = value[1];
+  this.setState({beginHour});
+  this.setState({endHour});
 
-  };
+}
+;
 
-{/*<h1 className={"bold-title"}>Active hours:</h1>*/}
-{/*<AirbnbSlider*/}
-    {/*ThumbComponent={AirbnbThumbComponent}*/}
-    {/*//ValueLabelComponent={ValueLabelComponent}*/}
-    {/*getAriaLabel={index => (index === 0 ? 'Begin active hours' : 'End active hours')}*/}
-    {/*defaultValue={[this.state.beginHour, this.state.endHour]}*/}
-    {/*value={[this.state.beginHour, this.state.endHour]}*/}
-    {/*valueLabelDisplay="auto"*/}
-    {/*min={0}*/}
-    {/*max={24}*/}
-    {/*marks={ACTIVE_MARKS}*/}
-    {/*onChange={(e,v) => this.handleSliderChange(e,v)}*/}
-  {/*/>*/}
+{/*<h1 className={"bold-title"}>Active hours:</h1>*/
+}
+{/*<AirbnbSlider*/
+}
+{/*ThumbComponent={AirbnbThumbComponent}*/
+}
+{/*//ValueLabelComponent={ValueLabelComponent}*/
+}
+{/*getAriaLabel={index => (index === 0 ? 'Begin active hours' : 'End active hours')}*/
+}
+{/*defaultValue={[this.state.beginHour, this.state.endHour]}*/
+}
+{/*value={[this.state.beginHour, this.state.endHour]}*/
+}
+{/*valueLabelDisplay="auto"*/
+}
+{/*min={0}*/
+}
+{/*max={24}*/
+}
+{/*marks={ACTIVE_MARKS}*/
+}
+{/*onChange={(e,v) => this.handleSliderChange(e,v)}*/
+}
+{/*/>*/
+}
