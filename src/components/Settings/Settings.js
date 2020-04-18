0cf5a12630b4a90f18044b0dc57e055f0a9e47ec
Merlin Laffitte <merlin.laffitte@student.ecp.fr>
2020-04-18 20:12:35 +0200

    Change parameter for beta test

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -32,8 +32,8 @@ const INIT_RELAXED_PROFILE = {
     audible: false,
   },
   scorer: {
-    min_active: 3 * 1000,
-    protection_time: 5 * 60 * 1000,
+    min_active: 5 * 60 * 1000,
+    protection_time: 15 * 60 * 1000,
     cached_decay: 0.7,
   },
 };
