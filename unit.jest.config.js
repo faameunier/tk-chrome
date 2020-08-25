80d1bb5a04fe2fe920bb8b365fff4b6fb95a3a04
Francois Meunier <francois.meunier@uber.com>
2020-08-25 16:55:54 +0200

    exclude conf from code coverage

--- unit.jest.config.js
+++ unit.jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
   // collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['src/**/*.js', '!src/**/*.style.js'],
+  collectCoverageFrom: ['src/**/*.js', '!src/**/*.style.js', '!src/config/*.js'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
